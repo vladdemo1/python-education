15020b756a486e23ec380d53d8624477dbada1c6
vladdemo1 <v.demchenko@student.csn.khai.edu>
2022-02-13 18:05:10 +0200

    Added 3 files

---
+++ bash/cron_time.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+real_time=$(date +"%D - %T")
+echo $real_time >> home_time.txt
---
+++ bash/get_weather.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -o /home/vladdemo/weather.json "https://api.openweathermap.org/data/2.5/weather?lat=50&lon=36&appid=e40511c1146863f7f58c75b9764ac294"
+# -o - it's a create new file with name weather.json
